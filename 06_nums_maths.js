ed44aa42d5601d2bb415edad1036ebc3614043e3
mukund_jha <jhamukund986@gmail.com>
2024-01-24 21:54:46 +0530

    commit

---
+++ 06_nums_maths.js
@@ -0,0 +1,97 @@
+const students_marks = 90
+console.log(students_marks)
+
+let Account_balance = new Number(80.09999)
+console.log(Account_balance)
+
+console.log(Account_balance.toString())
+
+console.log(typeof Account_balance)
+console.log(Account_balance .length)
+
+console.log(Account_balance.toFixed(4))
+console.log(Account_balance.toPrecision(5))
+
+let money = 1000000;
+
+
+
+
+console.log(money.toLocaleString('en-IN')) //indian precesion
+
+
+///=====maths///-----
+
+
+console.log(Math)
+
+let a = 4
+let b = -6
+
+console.log(Math.sqrt(a))
+
+console.log(Math.min(a,b))
+
+console.log(Math.sin(90))
+
+console.log(Math.max(a,b))
+
+console.log(Math.PI .toPrecision(6))
+
+console.log(Math.abs(b))
+
+const pi = 3.14142
+
+console.log(Math.round(pi))
+
+console.log(Math.ceil(pi))
+
+console.log(Math.floor(pi))
+
+console.log(Math.random())
+
+console.log((Math.random(pi)*10)+ 1);
+
+//=> random value always in between 0 to 1
+
+const min = 10
+
+const max = 20
+
+console.log(Math.floor(Math.random()  *(max - min +1)) + min )
+
+console.log(Math.floor(Math.random()  *(max - min +1)) + min )
+
+let number1 = 3
+let number2 = 4
+console.log(Math.hypot(number1,number2))
+
+
+console.log(Math.sign(-3))
+console.log(Math.sign(0))
+
+let float = 123.45
+console.log(Math.trunc(float))
+
+let ab = 2
+console.log(Math.pow(ab,2))
+
+console.log(Math.pow(ab,4))
+
+//basically .pow jo hotea haii wo power me deal krtea haii
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
