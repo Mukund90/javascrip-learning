d8499e0036812fd1f0529515e1c4d533238616fc
mukund_jha <jhamukund986@gmail.com>
2024-01-25 11:36:58 +0530

    commit

---
+++ 08_Array.js
@@ -0,0 +1,53 @@
+const myarr = ["mukundjha","satyamjha","muku","soni"]
+
+console.log(myarr[0,1,2,3,4,4])
+
+console.log(`The length of the array is->${myarr.length}`)
+
+console.log(myarr.slice(1,4))
+
+console.log(myarr)
+
+console.log(myarr.splice(1,2,"raghav","aman"))
+
+console.log(myarr)
+
+for(let i = 0 ;i<myarr.length-1;i++ )
+{
+    console.log(myarr[i])
+}
+
+const myarr1 = new Array("mukund",6,"public")
+
+console.log(myarr1.length)
+
+console.log(myarr1.toString())
+
+console.log(myarr1.push("mohan"))
+
+myarr1.sort()
+
+myarr1.pop()
+
+myarr1.unshift(10)
+
+myarr1.shift()
+
+console.log(myarr1)
+
+console.log(myarr1.includes("mukund"))
+
+console.log(myarr1.indexOf("mukund"))
+
+console.log(myarr1.at(2))
+
+console.log(myarr1)
+
+
+
+
+
+
+
+
+
