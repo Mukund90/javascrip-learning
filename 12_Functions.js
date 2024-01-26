98827fdb4a61cfab4130eae995cb9c8f9ab9c754
mukund_jha <jhamukund986@gmail.com>
2024-01-26 14:03:40 +0530

    function

---
+++ 12_Functions.js
@@ -0,0 +1,33 @@
+ function primenumber(a,b){ //[parameters]
+    return a*b;
+}
+
+let x =  primenumber(2,3)  //arguments
+
+console.log(x)
+
+
+
+function instagram_user(username = "anyone")
+{
+    if(username === undefined)
+    {
+        console.log("please Enter a valid username->")
+        return 
+
+    }
+
+   return `${username} just Logged in !!`
+
+}
+
+console.log(instagram_user())
+
+
+
+
+
+
+
+
+
