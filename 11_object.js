05b789ce490c62c1d2eae640aac71895218fe334
mukund_jha <jhamukund986@gmail.com>
2024-01-26 12:27:17 +0530

    json

---
+++ 11_object.js
@@ -0,0 +1,13 @@
+let instagram_user = {
+    user_name : "Mukund@986",
+    IsLoggIn :  "No",
+    user_password: "Mukund@2003" 
+}
+
+//destruction
+
+const {user_name,user_password,IsLoggIn}= instagram_user
+{
+console.log(`The Username is ${user_name} And the password is ${user_password} is user is login ${IsLoggIn}`)
+}
+
