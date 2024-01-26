e96ce9dfd189a25d6b1e5e11889d44d8c59b19f1
mukund_jha <jhamukund986@gmail.com>
2024-01-26 22:46:36 +0530

    iife

---
+++ 14_IIFE.js
@@ -0,0 +1,12 @@
+// Immediately invoked function expression(IIFE)
+(
+  fun1=function(){
+    //named IIFE
+  console.log(`Db is connected!`)
+})();
+
+(
+fun2 =(name)=>{
+    console.log(`Db2 is connected ${name}`);
+})("mukundjha")
+
